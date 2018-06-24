278b58a8f284369c0b0e77bdfe50e824de4bdc88
admin <brad@brad.tw>
2018-06-24 13:39:05 +0800

    2018062402

---
+++ src/gnumber.js
@@ -0,0 +1,17 @@
+var MainLayer = cc.Layer.extend({
+    sprite:null,
+    ctor:function () {
+        this._super();
+
+        return true;
+    }
+});
+
+var MainScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new MainLayer();
+        this.addChild(layer);
+    }
+});
+
