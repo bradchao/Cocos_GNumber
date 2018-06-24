d110a9cf6c49ad2266704c4a5152cdb0db1ea458
admin <brad@brad.tw>
2018-06-24 14:07:43 +0800

    2018062403

--- src/gnumber.js
+++ src/gnumber.js
@@ -1,10 +1,37 @@
 var MainLayer = cc.Layer.extend({
     sprite:null,
+    dx : 4,
     ctor:function () {
         this._super();
 
+        var title = new cc.LabelTTF("猜數字遊戲","",48);
+        title.x = cc.winSize.width / 2;
+        title.y = cc.winSize.height *7/8;
+        title.setColor(cc.color(255,255,0));
+        this.addChild(title,0,"mytitle");
+
+        this.initLayout();
+
+
+        this.scheduleUpdate();  // update()
+
         return true;
+    },
+
+    initLayout: function(){
+        
+    },
+
+    update: function(){
+        console.log("OK");
+        var title = this.getChildByName("mytitle");
+        if (title.x + title.width/2>= cc.winSize.width ||
+            title.x - title.width/2<=0){
+            this.dx *= -1;
+        }
+        title.x += this.dx;
     }
+
 });
 
 var MainScene = cc.Scene.extend({
