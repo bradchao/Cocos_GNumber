41fdf29f85bd6e8befd1f9b9c1984b40baa56654
admin <brad@brad.tw>
2018-06-30 12:18:07 +0800

    2018063003

--- src/resource.js
+++ src/resource.js
@@ -26,6 +26,8 @@ var res = {
     HelloWorld_png : "res/HelloWorld.png",
     back_png : "res/back.png",
     enter_png : "res/enter.png",
+    winner_png : "res/winner.png",
+    loser_png : "res/loser.png",
     number_plist : "res/number.plist",
     number_png : "res/number.png"
 };
